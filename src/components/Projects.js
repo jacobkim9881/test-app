54d3d3c78317089d610bbdaf6c8e1ff16f5a9d54
kim <jacobkim@gmail.com>
2019-11-22 15:39:41 +0900

    netlify

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,20 +2,31 @@ import React from 'react'
 import score from './pathfinderCalendar.json'
 
 const Projects = () => {
-    const projects = {
-        width: "750px"
+    const projects = {        
+        display: "flex",
+        flexWrap: "wrap",
+        width: "750px",
+        margin: "auto"        
     }
     const dates = {
-        display: "flex",
         width: "100px"
+        
+    }
+
+    const percent = {
+        color: "red"
     }
     return (
-        <div >      
+        <div>
+            <h2 style={{align:"center"}}>Pathfinder : Kingmaker</h2>
+            <h3 style={{align:"center"}}>November</h3>     
+        <div style={projects}>                        
                     {score.map(x =>                    
                     <p style={dates}>
-                    {x.date}<br />번역됨: result3 파일의 문장 12466개 중 {x.subs}개({Math.trunc(x.subs/12466*100)}%) 남음: {12466-x.subs}개
+                    {x.date}<br />번역됨: result3 파일의 문장 12466개 중 {x.subs}개(<a style={percent}>{Math.trunc(x.subs/12466*100)}%</a>) 남음: {12466-x.subs}개
                     </p>
                     )}
+        </div>
         </div>
     )
 }
