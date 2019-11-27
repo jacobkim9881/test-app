c0a574fc2c76e94b26e8150f14079271218058bb
kim <jacobkim@gmail.com>
2019-11-27 09:24:45 +0900

    forLambda

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 //import ThemeGiver from './ThemeGiver'
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: "/.netlify/functions/graphql"
+});
 
 //export const {Consumer, Provider} = React.createContext()
 
